9da648219227a76dc98b7786364ba75600667e0b
Simon Eddy <simon@simoneddy.com.au>
2023-05-16 17:36:36 +1000

    changed articles to pages

--- src/features/pages/pagesAPI.js
+++ src/features/pages/pagesAPI.js
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { API_URL } from '../../consts';
 
-export const articlesAPI = createApi({
-  baseQuery: fetchBaseQuery(`${API_URL}/articles`),
-  reducerPath: 'articlesAPI',
-  tagTypes: ['Article'],
+export const pagesAPI = createApi({
+  baseQuery: fetchBaseQuery(`${API_URL}/page`),
+  reducerPath: 'pagesAPI',
+  tagTypes: ['Page'],
   endpoints: (builder) => ({
-    fetchArticles: builder.query({
+    fetchPages: builder.query({
       query: () => '/',
       providesTags: (result, error) => {
         if (error) console.error(error);
         return (result
-          ? [...result.map(({ key }) => ({ type: 'Article', key })), 'Article']
-          : ['Article']);
+          ? [...result.map(({ key }) => ({ type: 'Page', key })), 'Page']
+          : ['Page']);
       }
     })
   })
